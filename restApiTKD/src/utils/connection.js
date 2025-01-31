6b18e5d78c3d812c7454d60cab399089ac18db4b
jhocepSan <jjchsan@gmail.com>
2025-01-31 12:11:22 -0400

    modificaciones io socket mandos

--- restApiTKD/src/utils/connection.js
+++
@@ -1,6 +0,0 @@
-import {createPool} from 'mysql2/promise';
-import {configuraciones} from '../config/config.js';
-import pgadmin from 'pg'
-
-export const pool=createPool(configuraciones.CONFIGDBLOCAL);
-export const poolPostgress = new pgadmin.Pool(configuraciones.CONFIGDBPG);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import {configuraciones} from './config/config.js'
+import { configuraciones } from './config/config.js'
 import app from './utils/app.js';
-import {pool,poolPostgress} from './utils/connection.js';
+import { pool, poolPostgress } from './utils/connection.js';
+import { PORT } from './config/configDeploy.js'
 import http from 'http';
-import {Server} from 'socket.io'
+import {WebSocketServer} from 'ws';
 
 
 async function InitServer() {
@@ -22,32 +23,39 @@ async function InitServer() {
         console.log("Error en la conneccion POSTGRESS: " + error);
         //await db.closeConnectionSC();
     }
-    
+
     try {
         /*console.log("Iniciando servidor ....")
         app.listen(app.get('port'),()=>{
             console.log('funcionando servidos en puerto '+ app.get('port'));
         });*/
-        const server = http.createServer(app)
-        const io = new Server(server,{
-            cors:{
-                methods:["GET","POST"]
-            }
-        })
-        io.on("connection",(socket)=>{
-            console.log(socket.id)
-            socket.on("join_room",(data)=>{
-                console.log(data);
-                socket.join(data)
-            })
-            socket.on("send_message",(data)=>{
-                console.log(data);
-                socket.to(data.room).emit("receive_message",data);
-            })
-        })
-        server.listen(app.get('port'), function () {
+        //const server = http.createServer(app)
+        const wss = new WebSocketServer({ port: app.get('port') });
+
+        // WebSocket event handling
+        wss.on('connection', (ws) => {
+            console.log('A new client connected.');
+
+            // Event listener for incoming messages
+            ws.on('message', (message) => {
+                console.log('Received message:', message.toString());
+
+                // Broadcast the message to all connected clients
+                wss.clients.forEach((client) => {
+                    if (client.readyState === 1) {
+                        client.send(message.toString());
+                    }
+                });
+            });
+
+            // Event listener for client disconnection
+            ws.on('close', () => {
+                console.log('A client disconnected.');
+            });
+        }); 
+        /*server.listen(app.get('port'), function () {
             console.log('funcionando servidos en puerto ' + app.get('port'));
-        });
+        });*/
     } catch (error) {
         console.log("Error iniciar Servidor : " + error)
     }
